5789610f14f8d3bedd425a581aa8b070a5aef057
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-08-11 02:37:09 +0330

    Add toggle theme option

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,14 +9,29 @@
             href="https://github.com/iAliF/Beautify"
             size="x-large"
         ></v-btn>
+
+        <v-btn
+            :icon="this.$vuetify.theme.current.dark ? 'mdi-white-balance-sunny' : 'mdi-weather-night'"
+            size="x-large"
+            @click="toggleTheme"
+        ></v-btn>
       </template>
     </v-app-bar>
   </div>
 </template>
 
 <script>
+import {useTheme} from 'vuetify'
+
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Navbar",
+  setup() {
+    const theme = useTheme()
+
+    return {
+      toggleTheme: () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+    }
+  }
 }
 </script>
