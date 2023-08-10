17a9761be15f0919fbc0487a26b912eee7612a0a
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-08-10 23:26:41 +0330

    Add input to the home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,25 @@
 <template>
-  <h1>Hey There</h1>
-  <p>Test</p>
-  <p>Test</p>
+  <div class="Home">
+    <v-row class="justify-center pt-16">
+      <v-col cols="8">
+        <v-text-field
+            label="Enter your text"
+            placeholder="Beautify"
+            v-model="textInput"
+            clearable
+            color="primary"
+            variant="outlined"
+        >
+        </v-text-field>
+      </v-col>
+    </v-row>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    textInput: ''
+  })
+}
+</script>
