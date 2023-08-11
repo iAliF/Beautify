cd01b76fce525ea4000e78153d4294717f97af38
Ali Fotouhi <mr.ali.fotouhi@gmail.com>
2023-08-11 04:27:55 +0330

    Add result to the home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Home">
-    <v-row class="justify-center pt-16">
+    <v-row class="justify-center mt-16">
       <v-col cols="8">
         <v-text-field
             label="Enter your text"
@@ -9,17 +9,62 @@
             clearable
             color="primary"
             variant="outlined"
+            @keyup="handleInput"
         >
         </v-text-field>
       </v-col>
     </v-row>
+
+    <v-row class="justify-center">
+      <v-col cols="8">
+        <v-card
+            class="pa-2"
+        rounded="xl"
+        >
+          <v-list
+              v-if="fontsList.length"
+              density="comfortable"
+          >
+            <v-list-subheader>Result</v-list-subheader>
+
+            <v-list-item
+                v-for="(value, index) in fontsList"
+                :key="index"
+                :value="value"
+                :title="value"
+                color="primary"
+                rounded="xl"
+                class="pa-4"
+            >
+              <template v-slot:append>
+                test
+              </template>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+
   </div>
 </template>
 
 <script>
+import beautify from "@/components/utils/fonts";
+
 export default {
   data: () => ({
-    textInput: ''
-  })
+    textInput: '',
+    fontsList: []
+  }),
+  methods: {
+    handleInput() {
+      if (!this.textInput || !this.textInput.length) {
+        this.fontsList = []
+        return
+      }
+
+      this.fontsList = beautify(this.textInput)
+    }
+  }
 }
 </script>
